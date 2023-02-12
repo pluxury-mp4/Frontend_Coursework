06f45d0f3d95069fa6518fd00b4475d92a284dd2
Max Blazhin <102875843+pluxury-mp4@users.noreply.github.com>
2023-02-12 16:58:56 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,1 @@
+<script language="JavaScript"> day = new Date(); hour = day.getHours(); if (hour>=5 && hour<12) greeting = "Доброе утро"; else { if (hour>=12 && hour<18) greeting = "Добрый день"; else { if (hour>=18 && hour<24) greeting = "Добрый вечер"; else { if (hour>=0 && hour<5) greeting = "Доброй ночи"; } } } document.write(greeting); 
